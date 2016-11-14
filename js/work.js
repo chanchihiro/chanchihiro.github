b56fec69bc8b2c8940907f8e3e7ab6e10e11cffe
chanchi- <chanchi0707@gmail.com>
2016-11-15 00:33:07 +0900

    タイミング調節！

--- js/work.js
+++ js/work.js
@@ -8,20 +8,20 @@ $(function(){
 	$window.on("scroll",function(){
 
 		///////////ロゴ
-		if($window.scrollTop() > logoOffsetTop - 1000 ){
+		if($window.scrollTop() > logoOffsetTop - 700 ){
 			$(".m1").animate({
 				"right" : 0
-			},2500);
+			},1000);
 		}
-		if($window.scrollTop() > textOffsetTop - 1000 ){
+		if($window.scrollTop() > textOffsetTop - 700 ){
 			$(".m2").animate({
 				"right" : 0
-			},2500);
+			},1000);
 		}
-		if($window.scrollTop() > imgOffsetTop - 1000 ){
+		if($window.scrollTop() > imgOffsetTop - 700 ){
 			$(".m3").animate({
 				"left" : 0
-			},2500);
+			},1000);
 		}
 
 
@@ -29,78 +29,78 @@ $(function(){
 
 
 		//////////イラスト
-		if($window.scrollTop() > $(".m4").offset().top - 600 ){
+		if($window.scrollTop() > $(".m4").offset().top - 700 ){
 			$(".m4").animate({
 				"right" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m5").offset().top - 450 ){
+		if($window.scrollTop() > $(".m5").offset().top - 700 ){
 			$(".m5").animate({
 				"left" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m6").offset().top - 500 ){
+		if($window.scrollTop() > $(".m6").offset().top - 700 ){
 			$(".m6").animate({
 				"left" : 0
-			},1800);
+			},1000);
 		}
 
 
 
 		///サイト
-		if($window.scrollTop() > $(".m7").offset().top - 600 ){
+		if($window.scrollTop() > $(".m7").offset().top - 700 ){
 			$(".m7").animate({
 				"right" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m8").offset().top - 450 ){
+		if($window.scrollTop() > $(".m8").offset().top - 700 ){
 			$(".m8").animate({
 				"right" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m9").offset().top - 500 ){
+		if($window.scrollTop() > $(".m9").offset().top - 700 ){
 			$(".m9").animate({
 				"left" : 0
-			},1800);
+			},1000);
 		}
 
 
 
 
 		////アプリケーション
-		if($window.scrollTop() > $(".m10").offset().top - 600 ){
+		if($window.scrollTop() > $(".m10").offset().top - 700 ){
 			$(".m10").animate({
 				"right" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m11").offset().top - 450 ){
+		if($window.scrollTop() > $(".m11").offset().top - 700 ){
 			$(".m11").animate({
 				"left" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m12").offset().top - 500 ){
+		if($window.scrollTop() > $(".m12").offset().top - 700 ){
 			$(".m12").animate({
 				"left" : 0
-			},1800);
+			},1000);
 		}
 
 
 
 		///マガジン
-		if($window.scrollTop() > $(".m13").offset().top - 600 ){
+		if($window.scrollTop() > $(".m13").offset().top - 700 ){
 			$(".m13").animate({
 				"right" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m14").offset().top - 450 ){
+		if($window.scrollTop() > $(".m14").offset().top - 700 ){
 			$(".m14").animate({
 				"right" : 0
-			},1800);
+			},1000);
 		}
-		if($window.scrollTop() > $(".m15").offset().top - 500 ){
+		if($window.scrollTop() > $(".m15").offset().top - 700 ){
 			$(".m15").animate({
 				"left" : 0
-			},1800);
+			},1000);
 		}
 	});
 });
