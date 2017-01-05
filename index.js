460034296caef56525887951ec65a2ab145efbfc
chanchi- <chanchi0707@gmail.com>
2017-01-06 01:17:44 +0900

    api読み込みの雛形の完成

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import jquery from 'jquery'
+
+
+class Blog extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data : []
+		};
+	}
+
+	loadAjax(){
+		$.ajax({
+			url : this.props.url,
+			dataType : 'json',
+			success : (data) =>{
+				this.setState({data : data});
+			},
+			error : (xhr, status, err) => {
+				console.error(this.props.url, status, err.toString());
+			}
+		});
+	}	
+}
+
+
+ReactDOM.render(
+	<Blog url='http://staff.hatenablog.com/entry/2014/08/29/141633' apikey=''/>,
+	document.getElementById("blog")
+);
